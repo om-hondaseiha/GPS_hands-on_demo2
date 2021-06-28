30435a3f13437bc5bcaa2b70f16b3bfaded11031
om-hondaseiha <honda.seiha.infratop@gmail.com>
2021-06-28 18:59:59 +0900

    [create] demo2(WIP)

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-function buttonClick(){
+$('#start').click(function(event){
+  $('body, html').animate({
+    scrollTop: 1100
+  }, 800);
+  event.preventDefault();
+});
+
+$('input').click(function(){
+  let className = '.' + $(this).attr('name');
+  $(className).removeClass('selected');
+  $(this).parent().addClass('selected');
+});
+
+$('#check').click(function(){
+  let faceType = $("input[name='face_type']");
+  let faceTypeLength = faceType.length;
+
+  let favorite = $("input[name='favorite']");
+  let favoriteLength = favorite.length;
+
   let faceTypeValue = '';
   let favoriteValue = '';
   let glassType = '';
 
   for (let i = 0; i < faceTypeLength; i++){
-    if (faceType.item(i).checked){
-      faceTypeValue = faceType.item(i).value;
+    if (faceType[i].checked){
+      faceTypeValue = faceType[i].value;
     }
-  }
+  };
   for (let j = 0; j < favoriteLength; j++){
-    if (favorite.item(j).checked){
-      favoriteValue = favorite.item(j).value;
+    if (favorite[j].checked){
+      favoriteValue = favorite[j].value;
     }
-  }
+  };
   // 輪郭4×好み2なので、以下のようにすると被らず8通りできる
   glassType = parseInt(faceTypeValue) * 2 + parseInt(favoriteValue);
-  diagnosis.style.backgroundImage = 'url(img/glass-' + glassType + '.png)';
-  diagnosis.style.border = '1px solid #000';
-  glassName.innerHTML = '結果は...メガネ' + glassType + '!!';
-}
 
-let faceType = document.getElementsByName('face_type');
-let faceTypeLength = faceType.length;
-
-let favorite = document.getElementsByName('favorite');
-let favoriteLength = favorite.length;
-
-let checkButton = document.getElementById('check');
-let diagnosis = document.getElementById('diagnosis');
-let glassName = document.getElementById('glass_name');
-
-checkButton.addEventListener('click', buttonClick);
+  $('#diagnosis').css({
+    'background-image' : 'url(img/glass-' + glassType + '.png)',
+    'border' : '1px solid #000'
+  });
+  $('#glass_name').html('結果は...メガネ' + glassType + '!!');
+});
